c0a992e9d09476d443f3ec77edda927fff199b4c
Gang Wu <ustcwg@gmail.com>
2015-04-24 00:06:40 -0400

    add helper files

---
+++ src/Common.h
@@ -0,0 +1,17 @@
+#pragma once
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+#define				BLOCK_SIZE_X		((int)64)
+#define				BLOCK_SIZE_Y		((int)64)
+#define				BLOCK_SIZE_Z		((int)8)
+#define				BLOCK_SIZE_BYTE		((int)(BLOCK_SIZE_X * BLOCK_SIZE_Y * BLOCK_SIZE_Z))
+
+typedef __int64           int64;
+typedef __int32           int32;
+typedef unsigned char	  uint8;
+typedef unsigned __int32  uint32;
+typedef unsigned __int16  uint16;
+typedef signed char       int8;
